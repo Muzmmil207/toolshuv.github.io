06bd6befd1b1310b24bc5c6b5e85f760061f7d2f
Bhuvnesh Raghav <81678480+bhuvnesh6@users.noreply.github.com>
2023-03-31 13:50:58 +0530

    Update like.php

--- like.php
+++ like.php
@@ -1,4 +1,4 @@
-<?php
+hp
 
 // Track the like click by incrementing the like count in a file or database
 
@@ -6,7 +6,22 @@ $likeCount = file_get_contents('like_count.txt');
 
 $likeCount++;
 
-file_put_contents('like_count.txt', $likeCount);
 
+
+
+
+<?php
+// Track the like click by incrementing the like count in a file or database
+$likeCount = file_get_contents('like_count.txt');
+$likeCount++;
+file_put_contents('like_count.txt', $likeCount);
+echo $likeCount;
 ?>
 
+<?php
+// Helper function to get the total like count
+function get_like_count() {
+  $likeCount = file_get_contents('like_count.txt');
+  return $likeCount;
+}
+?>
